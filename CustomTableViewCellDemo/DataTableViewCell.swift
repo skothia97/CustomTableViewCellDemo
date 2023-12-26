94374e2bf3ca8f3946ad21a3a10a3718041e6bec
Srikanta <srikantk021@gmail.com>
2023-12-26 23:08:09 +0530

    Initial Commit

---
+++ CustomTableViewCellDemo/DataTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DataTableViewCell.swift
+//  CustomTableViewCellDemo
+//
+//  Created by Srikanta Kumar Kothia on 26/12/23.
+//
+
+import UIKit
+
+class DataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
