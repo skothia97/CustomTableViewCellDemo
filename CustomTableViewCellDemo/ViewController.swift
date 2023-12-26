bc0645960514fdb146868726c794c69ea37def6d
Srikanta <srikantk021@gmail.com>
2023-12-26 23:09:57 +0530

    Initial message

--- CustomTableViewCellDemo/ViewController.swift
+++ CustomTableViewCellDemo/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tblView: UITableView!
+    
+    var arrayName = ["A","B","C"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tblView.register(UINib(nibName: "DataTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
     }
-
-
 }
+
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(arrayName.count)
+        return arrayName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! DataTableViewCell
+        cell.lblData.text = arrayName[indexPath.row]
+        return cell
+    }
+}
+
+
+
 
